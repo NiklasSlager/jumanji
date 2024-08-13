096440081c95cda6b61b2ec6c08eccf265849dd3
NiklasSlager <32905797+NiklasSlager@users.noreply.github.com>
2024-08-13 23:12:09 +0200

    Update env.py

--- jumanji/environments/distillation/env.py
+++ jumanji/environments/distillation/env.py
@@ -191,7 +191,7 @@ class Distillation(Environment[State, specs.DiscreteArray, Observation]):
                   "action_C2.2": next_state.stream.action[-1, -1, 1],
                   "action_C2.3": next_state.stream.action[-1, -1, 2],
                   "nr_product_streams": jnp.sum(jnp.max(next_state.stream.isproduct, axis=1)),
-                  "nr_columns": jnp.sum(next_state.overall_stream_actions),
+                  "nr_columns": jnp.sum(state.overall_stream_actions),
                   "iterations": iterator,
                   "converged": column_state.converged,
                   "outflow": jnp.sum(jnp.max(next_state.stream.isproduct*jnp.sum(next_state.stream.flows, axis=2), axis=1)),

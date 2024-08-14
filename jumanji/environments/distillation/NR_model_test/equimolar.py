81513b170cfb73d4aa68d3d3274e6dc27cd113cc
NiklasSlager <32905797+NiklasSlager@users.noreply.github.com>
2024-08-14 23:54:17 +0200

    Update equimolar.py

--- jumanji/environments/distillation/NR_model_test/equimolar.py
+++ jumanji/environments/distillation/NR_model_test/equimolar.py
@@ -1,158 +1,91 @@
 import jax.numpy as jnp
-from jax import jacfwd, vmap
-from jumanji.environments.distillation.NR_model_test.distillation_types import Tray, Mesh, State
-from jumanji.environments.distillation.NR_model_test import thermodynamics as thermo
-from jumanji.environments.distillation.NR_model_test.matrix_transforms import single_tuple_to_matrix
+import jax
+from jax import vmap, lax, jit
+from jumanji.environments.distillation.NR_model_test.distillation_types import State, NR_State, Trays, Tray, Thermo
+from jumanji.environments.distillation.NR_model_test import functions, initial_composition, matrix_transforms, jacobian, \
+    thermodynamics, costing, purity_constraint
+import os
 
-
-def m_function(state, tray_low, tray, tray_high, i, j):
-    return tray.l[i] + tray.v[i] - tray_high.l[i] - tray_low.v[i] - state.F[j]*state.z[i]
-
-
-def m_simple(state, tray, i, j):
-    eqmol_top = jnp.sum(tray.v)-state.distillate
-    eqmol_bottom = jnp.sum(tray.l)-(jnp.sum(state.F)-state.distillate)
-    eqmol = jnp.where(j < jnp.max(jnp.where(state.F > 0, jnp.arange(len(state.F)), 0)),
-                      jnp.sum(tray.v) - state.V[j],
-                      jnp.sum(tray.l) - state.L[j])
-    return eqmol
-
-
-def e_function(state: State, tray_low, tray, tray_high, i, j):
-    return ((tray.l[i]/jnp.sum(tray.l) * thermo.k_eq(tray.T, state.components[i], state.pressure) - tray.v[i]/jnp.sum(tray.v))) #*jnp.where(state.z > 0, 1, 0)[i]
+# from NR_model_test.analytic_jacobian import jacobian as pure_jac
+# from NR_model_test import analytic_jacobian
+import os
 
 
 
-def h_function(state: State, tray_low, tray, tray_high, j):
-    h_evap = thermo.h_evap(tray.T, 4)
-    h_vap = jnp.average(thermo.liquid_enthalpy(tray.T, state.components))
-    #-(jnp.sum(tray.v) - jnp.sum(tray.l) / state.RR)
-    cond_spec = jnp.where(state.light_key>0,
-                          (tray.v[state.light_key]-state.light_spec*jnp.sum(state.F)*state.z[state.light_key]),
-                          -(jnp.sum(tray.v) - jnp.sum(tray.l) / state.RR)
-                          )
-    reb_spec = jnp.where(state.heavy_key>0,
-                         (tray.l[state.heavy_key]-state.heavy_spec * jnp.sum(state.F)*state.z[state.heavy_key]),
-                         -(jnp.sum(tray.l) - (jnp.sum(state.F) - state.distillate))
-                         )
+def x_initial(state: State):
+    a, b, c = jacobian.g_jacobian_func(state)
+    g = vmap(g_sol, in_axes=(None, None, None, None, 0))(state, state.trays.low_tray, state.trays.tray,
+                                                         state.trays.high_tray, jnp.arange(len(state.trays.tray.T)))
+    dx = jnp.nan_to_num(functions.thomas(a, b, c, -1 * g, state.Nstages))
+    def min_res(t, state, dx):
+        dx_v = dx[:, :len(state.components)].transpose()
+        dx_l = dx[:, -len(state.components):].transpose()
+        dx_t = dx[:, len(state.components)].transpose()
 
-    result = jnp.where(j == 0, cond_spec, (
-        jnp.where(j == state.Nstages - 1, reb_spec, (
-                jnp.sum(tray.l * thermo.liquid_enthalpy(tray.T, state.components)) + jnp.sum(
-            tray.v * thermo.vapor_enthalpy(tray.T, state.components))
-                - jnp.sum(tray_high.l * thermo.liquid_enthalpy(tray_high.T, state.components)) - jnp.sum(
-            tray_low.v * thermo.vapor_enthalpy(tray_low.T, state.components)) - jnp.sum(state.F*state.Hfeed[j]))/(-h_vap))))
-    return result
+        v_new = (state.trays.tray.v + t *  dx_v)
+        l_new = (state.trays.tray.l + t * dx_l)
+        t_new = (state.trays.tray.T + t * dx_t)
+        max_t = functions.t_sat_solver(state.components, state.pressure)
+        max_t = jnp.where(state.z > 0, max_t, 0)
+        min_t = jnp.min(jnp.where((max_t >0) & (max_t != jnp.max(max_t)), max_t, jnp.max(max_t)-1))
+        v_new_final = jnp.where(v_new >= 0., v_new, state.trays.tray.v
+                                * jnp.exp(t * dx_v / jnp.where(state.trays.tray.v > 0, state.trays.tray.v, 1e30)))
+        l_new_final = jnp.where(l_new >= 0., l_new, state.trays.tray.l
+                                * jnp.exp(t * dx_l / jnp.where(state.trays.tray.l > 0, state.trays.tray.l, 1e30)))
+        t_new_final = jnp.where(t_new >= jnp.max(max_t),
+                                jnp.max(max_t),
+                                jnp.where(t_new <= jnp.min(min_t), jnp.min(min_t), t_new)
+                                )*jnp.where(state.temperature>0, 1, 0)
+        state = state.replace(
+            Y=jnp.nan_to_num(v_new_final / jnp.sum(v_new_final, axis=0)),
+            X=jnp.nan_to_num(l_new_final / jnp.sum(l_new_final, axis=0)),
+            temperature=t_new_final,
+        )
+
+        state = matrix_transforms.trays_func(state)
+
+        g = vmap(g_sol, in_axes=(None, None, None, None, 0))(state, state.trays.low_tray, state.trays.tray,
+                                                                 state.trays.high_tray,
+                                                                 jnp.arange(len(state.trays.tray.T)))
+
+        return jnp.nan_to_num(jnp.sum(g ** 2), nan=1e10), state
+
+    carry = vmap(min_res, in_axes=(0, None, None))(jnp.arange(0.7, 1.3, 0.1), state, dx)
+    result, states = carry
+    new_t = jnp.max(jnp.where(result == jnp.min(result), jnp.arange(0.7, 1.3, 0.1), 0))
+
+    res, state_new = min_res(new_t, state, dx)
+    return res, state_new
 
 
-def f_vector_function(state: State, tray_low, tray, tray_high, j):
-    h = jnp.asarray(jnp.where(j < state.Nstages, h_function(state, tray_low, tray, tray_high, j), 0))
-    m = jnp.asarray(jnp.where(j < state.Nstages, vmap(m_function, in_axes=(None, None, None, None, 0, None))(state, tray_low, tray, tray_high, jnp.arange(len(state.components)), j), 0))
-    e = jnp.asarray(jnp.where(j < state.Nstages, vmap(e_function, in_axes=(None, None, None, None, 0, None))(state, tray_low, tray, tray_high, jnp.arange(len(state.components)), j), 0))
-    return Mesh(H=h,
-                M=m,
-                E=e,
-                )
+def cond_fn(args):
+    state, iterations, res = args
+    comps = jnp.sum(jnp.where(state.z > 0, 1, 0))
+    cond = state.Nstages * (2 * comps + 1) * jnp.sum(state.F) * 1e-9
+    return (iterations < 100) & (res > cond)
 
-def g_vector_function(state: State, tray_low, tray, tray_high, j):
-    eqmol_top = jnp.sum(tray.v)-state.distillate
-    eqmol_bottom = jnp.sum(tray.l)-(jnp.sum(state.F)-state.distillate)
-    '''
-    eqmol = jnp.where(j < jnp.max(jnp.where(state.F > 0, jnp.arange(len(state.F)), 0)),
-                      jnp.sum(tray.v)/(state.RR+1) - jnp.sum(tray.l)/state.RR,
-                      jnp.sum(tray.v)/(state.RR+1) - (jnp.sum(tray.l) - jnp.sum(state.F))/state.RR)
-    '''
-    eqmol = jnp.sum(tray.l)-state.L[j]
-    m = jnp.asarray(jnp.where(j < state.Nstages, vmap(m_function, in_axes=(None, None, None, None, 0, None))(state, tray_low, tray, tray_high, jnp.arange(len(state.components)), j), 0))
-    h = eqmol
-    e = jnp.asarray(jnp.where(j < state.Nstages, vmap(e_function, in_axes=(None, None, None, None, 0, None))(state, tray_low, tray, tray_high, jnp.arange(len(state.components)), j), 0))
 
-    return Mesh(H=h,
-                M=m,
-                E=e,
-                )
+def body_fn(args):
+    state, iterations, res = args
+    res_new, nr_state_new = x_initial(state)
+    nr_state_new = nr_state_new.replace(residuals=nr_state_new.residuals.at[iterations].set(res_new))
+    iterations += 1
+    return nr_state_new, iterations, res_new
 
-def f_jac_a(state: State, tray_low, tray, tray_high, j):
-    a = jacfwd(f_vector_function, argnums=3)(
-        state,
-        Tray(l=tray_low.l[:, j], v=tray_low.v[:, j], T=tray_low.T[j]),
-        Tray(l=tray.l[:, j], v=tray.v[:, j], T=tray.T[j]),
-        Tray(l=tray_high.l[:, j], v=tray_high.v[:, j], T=tray_high.T[j]),
-        j,
+
+def converge_equimolar(state: State):
+    # nr_state = initialize_NR(state)x
+
+    state = matrix_transforms.trays_func(state)
+    iterations = 0
+    res = 0
+
+    state, iterations, res = (
+        lax.while_loop(cond_fun=cond_fn, body_fun=body_fn,
+                       init_val=(state,
+                                 iterations,
+                                 jnp.array(10, dtype=float),
+                                 )
+                       )
     )
-    return single_tuple_to_matrix(a)
-
-
-def f_jac_b(state: State, tray_low, tray, tray_high, j):
-    b = jacfwd(f_vector_function, argnums=2)(
-        state,
-        Tray(l=tray_low.l[:, j], v=tray_low.v[:, j], T=tray_low.T[j]),
-        Tray(l=tray.l[:, j], v=tray.v[:, j], T=tray.T[j]),
-        Tray(l=tray_high.l[:, j], v=tray_high.v[:, j], T=tray_high.T[j]),
-        j,
-    )
-    return single_tuple_to_matrix(b)
-
-
-def f_jac_c(state: State, tray_low, tray, tray_high, j):
-    c = jacfwd(f_vector_function, argnums=1)(
-        state,
-        Tray(l=tray_low.l[:, j], v=tray_low.v[:, j], T=tray_low.T[j]),
-        Tray(l=tray.l[:, j], v=tray.v[:, j], T=tray.T[j]),
-        Tray(l=tray_high.l[:, j], v=tray_high.v[:, j], T=tray_high.T[j]),
-        j,
-    )
-    return single_tuple_to_matrix(c)
-
-
-def g_jac_a(state: State, tray_low, tray, tray_high, j):
-    a = jacfwd(g_vector_function, argnums=3)(
-        state,
-        Tray(l=tray_low.l[:, j], v=tray_low.v[:, j], T=tray_low.T[j]),
-        Tray(l=tray.l[:, j], v=tray.v[:, j], T=tray.T[j]),
-        Tray(l=tray_high.l[:, j], v=tray_high.v[:, j], T=tray_high.T[j]),
-        j,
-    )
-    return single_tuple_to_matrix(a)
-
-
-def g_jac_b(state: State, tray_low, tray, tray_high, j):
-    b = jacfwd(g_vector_function, argnums=2)(
-        state,
-        Tray(l=tray_low.l[:, j], v=tray_low.v[:, j], T=tray_low.T[j]),
-        Tray(l=tray.l[:, j], v=tray.v[:, j], T=tray.T[j]),
-        Tray(l=tray_high.l[:, j], v=tray_high.v[:, j], T=tray_high.T[j]),
-        j,
-    )
-    return single_tuple_to_matrix(b)
-
-
-def g_jac_c(state: State, tray_low, tray, tray_high, j):
-    c = jacfwd(g_vector_function, argnums=1)(
-        state,
-        Tray(l=tray_low.l[:, j], v=tray_low.v[:, j], T=tray_low.T[j]),
-        Tray(l=tray.l[:, j], v=tray.v[:, j], T=tray.T[j]),
-        Tray(l=tray_high.l[:, j], v=tray_high.v[:, j], T=tray_high.T[j]),
-        j,
-    )
-    return single_tuple_to_matrix(c)
-
-
-def jacobian_func(state: State):
-    b = vmap(f_jac_b, in_axes=(None, None, None, None, 0))(state, state.trays.low_tray, state.trays.tray,
-                                                           state.trays.high_tray, jnp.arange(len(state.trays.tray.T)))
-    a = vmap(f_jac_a, in_axes=(None, None, None, None, 0))(state, state.trays.low_tray, state.trays.tray,
-                                                           state.trays.high_tray, jnp.arange(len(state.trays.tray.T)))
-    c = vmap(f_jac_c, in_axes=(None, None, None, None, 0))(state, state.trays.low_tray, state.trays.tray,
-                                                           state.trays.high_tray, jnp.arange(len(state.trays.tray.T)))
-    return a[1:, :, :], b, c[:-1, :, :]
-
-
-def g_jacobian_func(state: State):
-    b = vmap(g_jac_b, in_axes=(None, None, None, None, 0))(state, state.trays.low_tray, state.trays.tray,
-                                                           state.trays.high_tray, jnp.arange(len(state.trays.tray.T)))
-    a = vmap(g_jac_a, in_axes=(None, None, None, None, 0))(state, state.trays.low_tray, state.trays.tray,
-                                                           state.trays.high_tray, jnp.arange(len(state.trays.tray.T)))
-    c = vmap(g_jac_c, in_axes=(None, None, None, None, 0))(state, state.trays.low_tray, state.trays.tray,
-                                                           state.trays.high_tray, jnp.arange(len(state.trays.tray.T)))
-    return a[1:, :, :], b, c[:-1, :, :]
+    return state, iterations, res
